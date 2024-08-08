6164d2507ae3ee37ba49123bf025a214ef864efa
Gabriela Schaper <gabrielasch2409@gmail.com>
2024-08-08 17:10:59 -0300

    Add files via upload

---
+++ Esfera.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace __ATIVIDADE_AVALIATIVA___5_PONTOS
+{
+    public partial class Atividade_10_pag_16 : Form
+    {
+        public Atividade_10_pag_16()
+        {
+            InitializeComponent();
+        }
+
+        private void btnok_Click(object sender, EventArgs e)
+        {
+            
+            int raio = int.Parse(txtraio.Text);
+
+            MessageBox.Show(( Math.PI * (raio*raio*raio) * 4 / 3 ) .ToString());
+
+        }
+    }
+}
